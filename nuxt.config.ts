0b278c8be97b1829028f90882fb88c203b6dc417
妙码生花 <18523774412@qq.com>
2023-02-09 23:54:06 +0800

    feat:基本信息和基本配置初始化

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
+import pkg from './package.json'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: pkg.name,
+            meta: [{ charset: 'utf-8' }, { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+        },
+    },
     typescript: {
         shim: false,
     },
