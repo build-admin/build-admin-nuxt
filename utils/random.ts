6332e0f3193b5110b582dfafca9a9b3f6229e00f
妙码生花 <18523774412@qq.com>
2023-02-12 02:58:08 +0800

    refactor:调整全局变量的使用

--- utils/random.ts
+++ utils/random.ts
@@ -49,9 +49,8 @@ export function uuid(): string {
  * @returns 唯一标识
  */
 export function shortUuid(prefix = ''): string {
-    const globals = useGlobalsStore()
     const time = Date.now()
     const random = Math.floor(Math.random() * 1000000000)
-    globals.unique++
-    return (prefix ? prefix + '_' : '') + random + globals.unique + String(time)
+    globals.unique.value++
+    return (prefix ? prefix + '_' : '') + random + globals.unique.value + String(time)
 }
