62616162e7b889b8d55961866b86fa7bed215b8e
妙码生花 <18523774412@qq.com>
2023-02-23 23:32:11 +0800

    feat:会员中心布局初始化、样式优化

--- layouts/user.vue
+++ layouts/user.vue
@@ -1,9 +1,51 @@
 <template>
     <div>
-        <slot />
+        <el-container class="is-vertical">
+            <BaHeader />
+            <el-scrollbar :style="mainScrollbarHeight()" ref="mainScrollbarRef">
+                <el-row class="layouts-main" justify="center">
+                    <el-col class="user-layouts" :span="16" :xs="24">
+                        <!-- <BaAside class="hidden-sm-and-down" /> -->
+                        <el-main class="layout-main">
+                            <slot />
+                        </el-main>
+                    </el-col>
+                </el-row>
+                <BaFooter />
+            </el-scrollbar>
+        </el-container>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { CSSProperties } from 'vue'
 
-<style scoped lang="scss"></style>
+/**
+ * main高度
+ * @param extra main高度额外减去的px数,可以实现隐藏原有的滚动条
+ * @returns CSSProperties
+ */
+function mainScrollbarHeight(extra = 0): CSSProperties {
+    let height = extra + 60
+    return {
+        height: 'calc(100vh - ' + height.toString() + 'px)',
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.user-layouts {
+    display: flex;
+    padding-top: 15px;
+    align-items: flex-start;
+}
+.layout-main {
+    padding: 0 !important;
+    overflow-x: hidden;
+}
+@media screen and (max-width: 768px) {
+    .user-layouts {
+        padding-top: 0;
+    }
+}
+</style>
