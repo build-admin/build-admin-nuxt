11be015ece6c43a5df4789fe9fc6acf7560f38f4
妙码生花 <18523774412@qq.com>
2023-02-25 02:42:09 +0800

    feat:在上传扩展中增加成功上传的返回示例

--- composables/mixins/baUpload.ts
+++ composables/mixins/baUpload.ts
@@ -1,4 +1,5 @@
 import type { FetchError } from 'ofetch'
+import { ElNotification } from 'element-plus'
 import { _AsyncData } from 'nuxt/dist/app/composables/asyncData'
 
 export const state: () => 'disable' | 'enable' = () => 'disable'
@@ -7,6 +8,24 @@ export function fileUpload<DataT = any>(fd: FormData, params: anyObj = {}): Prom
     // 上传扩展，定义此函数，并将上方的 state 设定为 enable，系统可自动使用此函数进行上传
     return new Promise((resolve, reject) => {
         console.log(fd, params)
+        ElNotification({
+            type: 'error',
+            message: '上传扩展未定义！',
+        })
         reject('未定义')
+
+        // 成功上传示例
+        const res = {
+            code: 1,
+            data: {
+                file: {
+                    full_url: fullUrl('file_url'),
+                    url: 'file_url',
+                },
+            },
+            msg: '',
+            time: Date.now(),
+        }
+        resolve({ data: ref(res) } as unknown as _AsyncData<ApiResponse<DataT>, FetchError | null>)
     })
 }
