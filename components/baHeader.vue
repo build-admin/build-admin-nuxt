23b5c709baec772b2f8cc34f2910fba45984ce80
妙码生花 <18523774412@qq.com>
2023-07-25 09:04:36 +0800

    style:优化顶栏样式

--- components/baHeader.vue
+++ components/baHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <el-header class="ba-header">
         <el-row justify="center">
-            <el-col class="header-row" :span="16" :xs="24">
+            <el-col class="header-row" :xs="24" :sm="24" :md="16">
                 <client-only>
                     <div @click="navigateTo({ name: '/' })" class="header-logo hidden-sm-and-down">
                         <img src="~/assets/images/logo.png" />
@@ -17,58 +17,60 @@
 
                     <!-- globals.menu.show 解决切换路由时菜单内部的 el-popper 报警告的问题 -->
                     <!-- Slot "default" invoked outside of the render function -->
-                    <el-menu
-                        v-if="globals.menu.show"
-                        :default-active="state.activeMenu"
-                        class="frontend-header-menu"
-                        mode="horizontal"
-                        :ellipsis="false"
-                    >
-                        <el-menu-item @click="navigateTo({ name: '/' })" v-blur index="index">{{ $t('Home') }}</el-menu-item>
+                    <el-scrollbar>
+                        <el-menu
+                            v-if="globals.menu.show"
+                            :default-active="state.activeMenu"
+                            class="frontend-header-menu"
+                            mode="horizontal"
+                            :ellipsis="false"
+                        >
+                            <el-menu-item @click="navigateTo({ name: '/' })" v-blur index="index">{{ $t('Home') }}</el-menu-item>
 
-                        <!-- 动态菜单 -->
-                        <MenuSub :menus="siteConfig.headNav" />
+                            <!-- 动态菜单 -->
+                            <MenuSub :menus="siteConfig.headNav" />
 
-                        <template v-if="memberCenter.state.open">
-                            <el-sub-menu v-if="userInfo.isLogin()" v-blur index="user-box">
-                                <template #title>
-                                    <div class="header-user-box">
-                                        <img
-                                            class="header-user-avatar"
-                                            :src="fullUrl(userInfo.avatar ? userInfo.avatar : '/static/images/avatar.png')"
-                                            alt=""
-                                        />
-                                        {{ userInfo.nickname }}
-                                    </div>
-                                </template>
-                                <el-menu-item @click="navigateTo({ name: 'user' })" v-blur index="user">
+                            <template v-if="memberCenter.state.open">
+                                <el-sub-menu v-if="userInfo.isLogin()" v-blur index="user-box">
+                                    <template #title>
+                                        <div class="header-user-box">
+                                            <img
+                                                class="header-user-avatar"
+                                                :src="fullUrl(userInfo.avatar ? userInfo.avatar : '/static/images/avatar.png')"
+                                                alt=""
+                                            />
+                                            {{ userInfo.nickname }}
+                                        </div>
+                                    </template>
+                                    <el-menu-item @click="navigateTo({ name: 'user' })" v-blur index="user">
+                                        {{ $t('Member Center') }}
+                                    </el-menu-item>
+                                    <!-- 动态菜单 -->
+                                    <MenuSub :menus="memberCenter.state.navUserMenus" />
+                                    <el-menu-item @click="userInfo.logout()" v-blur index="user-logout">{{ $t('Logout login') }}</el-menu-item>
+                                </el-sub-menu>
+                                <el-menu-item v-else @click="navigateTo({ name: 'user' })" v-blur index="user">
                                     {{ $t('Member Center') }}
                                 </el-menu-item>
-                                <!-- 动态菜单 -->
-                                <MenuSub :menus="memberCenter.state.navUserMenus" />
-                                <el-menu-item @click="userInfo.logout()" v-blur index="user-logout">{{ $t('Logout login') }}</el-menu-item>
-                            </el-sub-menu>
-                            <el-menu-item v-else @click="navigateTo({ name: 'user' })" v-blur index="user">
-                                {{ $t('Member Center') }}
-                            </el-menu-item>
 
-                            <el-sub-menu v-blur index="switch-language">
-                                <template #title>{{ $t('Language') }}</template>
-                                <el-menu-item
-                                    @click="setLanguage(item.name as Locales)"
-                                    v-for="item in languageList"
-                                    :key="item.name"
-                                    :index="'switch-language-' + item.value"
-                                >
-                                    {{ item.value }}
+                                <el-sub-menu v-blur index="switch-language">
+                                    <template #title>{{ $t('Language') }}</template>
+                                    <el-menu-item
+                                        @click="setLanguage(item.name as Locales)"
+                                        v-for="item in languageList"
+                                        :key="item.name"
+                                        :index="'switch-language-' + item.value"
+                                    >
+                                        {{ item.value }}
+                                    </el-menu-item>
+                                </el-sub-menu>
+
+                                <el-menu-item index="theme-switch" class="theme-switch">
+                                    <DarkSwitch @click="setDark(!getDark())" />
                                 </el-menu-item>
-                            </el-sub-menu>
-
-                            <el-menu-item index="theme-switch" class="theme-switch">
-                                <DarkSwitch @click="setDark(!getDark())" />
-                            </el-menu-item>
-                        </template>
-                    </el-menu>
+                            </template>
+                        </el-menu>
+                    </el-scrollbar>
                 </client-only>
             </el-col>
         </el-row>
@@ -138,69 +140,72 @@ watch(
 .ba-header {
     background-color: var(--ba-bg-color-overlay);
     box-shadow: 0 0 8px rgba(0 0 0 / 8%);
-}
-.header-row {
-    display: flex;
-}
-.user-menus-expand {
-    display: flex;
-    height: 60px;
-    align-items: center;
-    justify-content: center;
-    padding-left: 15px;
-}
-.header-logo {
-    display: flex;
-    height: 60px;
-    align-items: center;
-    cursor: pointer;
-    img {
-        height: 34px;
-        width: 34px;
+    .header-row {
+        display: flex;
+        justify-content: space-between;
     }
-    span {
-        padding-left: 4px;
-        font-size: var(--el-font-size-large);
+    .user-menus-expand {
+        display: flex;
+        height: var(--el-header-height);
+        align-items: center;
+        justify-content: center;
     }
-}
-.switch-language {
-    display: flex;
-    align-items: center;
-    span {
-        padding-right: 4px;
+    .header-user-box {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        .header-user-avatar {
+            width: 16px;
+            height: 16px;
+            margin-right: 4px;
+            border-radius: 50%;
+        }
+    }
+    .theme-switch {
+        --el-menu-hover-bg-color: none;
+        padding-right: 0;
+    }
+    .header-logo {
+        display: flex;
+        height: var(--el-header-height);
+        align-items: center;
+        padding-right: 15px;
+        cursor: pointer;
+        img {
+            height: 34px;
+            width: 34px;
+        }
+        .site-name {
+            padding-left: 4px;
+            font-size: var(--el-font-size-large);
+            white-space: nowrap;
+        }
+    }
+    .frontend-header-menu {
+        height: var(--el-header-height);
+    }
+    .switch-language {
+        display: flex;
+        align-items: center;
+        span {
+            padding-right: 4px;
+        }
     }
 }
 .el-menu--horizontal {
     margin-left: auto;
     border-bottom: none;
 }
-.header-user-box {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: relative;
-    .header-user-avatar {
-        width: 16px;
-        height: 16px;
-        margin-right: 4px;
-        border-radius: 50%;
-    }
-}
 .el-menu--horizontal > .el-menu-item,
 .el-menu--horizontal > :deep(.el-sub-menu) .el-sub-menu__title,
 .el-menu--horizontal > .el-menu-item.is-active {
     border-bottom: none;
 }
-.theme-switch {
-    --el-menu-hover-bg-color: none;
-}
 @at-root html.dark {
     .header-logo .site-name {
         color: var(--el-text-color-primary);
     }
-}
-.theme-switch {
-    padding-right: 0;
 }
 @media screen and (max-width: 768px) {
     .user-menus-expand {
