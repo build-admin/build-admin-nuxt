b2c490002a24cf89175b1612581a9d98823f1eb1
妙码生花 <18523774412@qq.com>
2024-12-31 00:01:13 +0800

    fix(BaInput):修复 number 输入框值为 null 会自动被转为 0 的问题

--- components/baInput/index.vue
+++ components/baInput/index.vue
@@ -1,5 +1,5 @@
 <script lang="ts">
-import { isArray } from 'lodash-es'
+import { isArray, isString } from 'lodash-es'
 import type { PropType, VNode } from 'vue'
 import { createVNode } from 'vue'
 import { getArea } from '~/api/common'
@@ -251,7 +251,7 @@ export default defineComponent({
                                 class: 'w100',
                                 'controls-position': 'right',
                                 ...attrs.value,
-                                modelValue: Number(props.modelValue),
+                                modelValue: isString(props.modelValue) ? Number(props.modelValue) : props.modelValue,
                                 'onUpdate:modelValue': onValueUpdate,
                             },
                             slots
